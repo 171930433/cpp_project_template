63a5d034507a678d8af0b2ce2ade5f1950677bac
guanshoukui <guanshoukui@zhito.com>
2024-09-26 07:50:42 +0000

    1. 调整目录结构 2. 调整安装
    Change-Id: I42bd636cc263daa634acdb1f7ec63be475eb75dd

---
+++ src/mylib.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "export.h"
+#include "message/message_buffer.h"
+
+class MYLIB_EXPORT MultuiSensorFusion {
+public:
+  template <typename _Message>
+  void Append(std::shared_ptr<ChannelMsg<_Message>> frame) {
+    buffer_.Append(frame);
+  }
+
+protected:
+  TotalBuffer buffer_;
+};
+
+#ifdef HAVE_THIRD_PARTY
+#include <ylt/struct_json/json_reader.h>
+#include <ylt/struct_json/json_writer.h>
+#endif
+
+int MYLIB_EXPORT add(int const x, int const y);
+int MYLIB_NO_EXPORT minus(int const x, int const y);
+
+#ifdef HAVE_THIRD_PARTY
+
+template <typename _T>
+inline std::string to_string(_T const& elem) {
+  std::string str;
+  struct_json::to_json(elem, str); // {"name":"tom","age":20}
+  return str;
+}
+
+#endif

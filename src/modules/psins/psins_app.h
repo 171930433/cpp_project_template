be32119d71f45875e8f0451c51febb0571451b99
gsk <171930433@qq.com>
2024-10-02 11:33:29 +0800

    psins编译完成

---
+++ src/modules/psins/psins_app.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "PSINSCore/PSINS.h"
+#include "PSINSCore/KFApp.h"
+#include "modules/app_base.h"
+
--- src/mylib.cpp
+++ src/mylib.cpp
@@ -1,4 +1,5 @@
 #include "mylib.h"
+#include "modules/app_base.h"
 
 DEFINE_string(config_dir, "", "config dir");
 

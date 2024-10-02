22cd5dab9c696269a598e9311da471dfc794f8fb
gsk <171930433@qq.com>
2024-10-02 23:17:34 +0800

    implot集成完毕

--- src/my_app.cpp
+++ src/my_app.cpp
@@ -1,18 +1,37 @@
-#include "hello_imgui/hello_imgui.h"
+#include <hello_imgui/hello_imgui.h>
+#include <implot.h>
 
-HelloImGui::RunnerParams InitRunnerParams();
+struct MyRunnerParams : public HelloImGui::RunnerParams {
+  MyRunnerParams();
 
+protected:
+  bool show_imgui_demo_ = true;
+  bool show_implot_demo_ = true;
+};
 
 int main(int, char*[]) {
 
-  auto params = InitRunnerParams();
+  MyRunnerParams params;
   HelloImGui::Run(params);
 }
 
-HelloImGui::RunnerParams InitRunnerParams() {
-  HelloImGui::RunnerParams para;
-
+MyRunnerParams::MyRunnerParams() {
+  auto& para = *this;
   auto& callbacks = para.callbacks;
+  callbacks.PostInit = [] { ImPlot::CreateContext(); };
+  callbacks.BeforeExit_PostCleanup = [] {
+    if (ImPlot::GetCurrentContext()) { ImPlot::DestroyContext(); }
+  };
+
+  callbacks.ShowAppMenuItems = [this] {
+    ImGui::MenuItem("Imgui::DemoWindow", nullptr, &show_imgui_demo_);
+    ImGui::MenuItem("Implot::DemoWindow", nullptr, &show_implot_demo_);
+  };
+
+  callbacks.BeforeImGuiRender = [this] {
+    if (show_imgui_demo_) { ImGui::ShowDemoWindow(&show_imgui_demo_); }
+    if (show_implot_demo_) { ImPlot::ShowDemoWindow(&show_implot_demo_); }
+  };
 
   // appWindowParams
   para.appWindowParams.windowTitle = "my Example";
@@ -36,10 +55,8 @@ HelloImGui::RunnerParams InitRunnerParams() {
 
   //   dock
   auto& dock = para.dockingParams;
-  HelloImGui::DockableWindow dw1("dw window1","name1");
-  HelloImGui::DockableWindow dw2("dw window2","name2");
+  HelloImGui::DockableWindow dw1("dw window1", "name1");
+  HelloImGui::DockableWindow dw2("dw window2", "name2");
   dock.dockableWindows.push_back(dw1);
   dock.dockableWindows.push_back(dw2);
-
-  return para;
 }

7a958610ee5ca82e6132db5d9520dfcdf73ae8f1
gsk <171930433@qq.com>
2024-10-07 10:54:41 +0800

    添加消息特化

--- tests/sensors.cpp
+++ tests/sensors.cpp
@@ -23,7 +23,6 @@ TEST(sensors, channel_message) {
   auto imu = Message<Imu>::Create("/imu");
   auto gnss = Message<Gnss>::Create("/gnss");
 
-
   EXPECT_TRUE(imu);
   EXPECT_TRUE(gnss);
 }
@@ -36,7 +35,6 @@ TEST(sensors, cache_message) {
   std::vector<MessageBase::SPtr> buffer;
   buffer.push_back(imu);
   buffer.push_back(gnss);
-
 
   EXPECT_TRUE(buffer.size() == 2);
 }
@@ -83,14 +81,10 @@ TEST_F(MessageBufferTest, base) {
 TEST_F(MessageBufferTest, order_pushback) {
 
   // push back order
-  std::vector<double> true_order{ imu2->t0(), gnss2->t0(), gnss->t0(),
-    imu->t0() };
+  std::vector<double> true_order{ imu2->t0(), gnss2->t0(), gnss->t0(), imu->t0() };
   std::vector<double> test_order;
-  for (auto const& elem : buffer.get<id>()) {
-    test_order.push_back(elem->t0());
-  }
-  GTEST_LOG_(INFO) << "order push_bask: "
-            << fmt::format("{:.3f}", fmt::join(test_order, ", "));
+  for (auto const& elem : buffer.get<id>()) { test_order.push_back(elem->t0()); }
+  GTEST_LOG_(INFO) << "order push_bask: " << fmt::format("{:.3f}", fmt::join(test_order, ", "));
   EXPECT_EQ(true_order, test_order);
 }
 
@@ -98,23 +92,47 @@ TEST_F(MessageBufferTest, order_t1) {
 
   // t1
   std::vector<double> test_order;
-  std::vector<double> true_order_t1{ imu->t1_, imu2->t1_, gnss->t1_,
-    gnss2->t1_ };
+  std::vector<double> true_order_t1{ imu->t1_, imu2->t1_, gnss->t1_, gnss2->t1_ };
   for (auto const& elem : buffer.get<t1>()) { test_order.push_back(elem->t1_); }
-  GTEST_LOG_(INFO) << "order t1: "
-            << fmt::format("{:.3f}", fmt::join(test_order, ", "));
+  GTEST_LOG_(INFO) << "order t1: " << fmt::format("{:.3f}", fmt::join(test_order, ", "));
   EXPECT_EQ(true_order_t1, test_order);
 }
 
 TEST_F(MessageBufferTest, order_t0) {
   // t0
   std::vector<double> test_order;
-  std::vector<double> true_order_t0{ imu->t0(), gnss->t0(), imu2->t0(),
-    gnss2->t0() };
-  for (auto const& elem : buffer.get<t0>()) {
-    test_order.push_back(elem->t0());
-  }
-  GTEST_LOG_(INFO) << "order t0: "
-            << fmt::format("{:.3f}", fmt::join(test_order, ", "));
+  std::vector<double> true_order_t0{ imu->t0(), gnss->t0(), imu2->t0(), gnss2->t0() };
+  for (auto const& elem : buffer.get<t0>()) { test_order.push_back(elem->t0()); }
+  GTEST_LOG_(INFO) << "order t0: " << fmt::format("{:.3f}", fmt::join(test_order, ", "));
   EXPECT_EQ(true_order_t0, test_order);
+}
+
+template <typename _T>
+struct IsTraject : public std::false_type {};
+
+template <>
+struct IsTraject<Gnss> : public std::true_type {};
+
+template <typename _T, bool = IsTraject<_T>::value>
+struct InnerMessage;
+
+template <typename _T>
+struct InnerMessage<_T, false> {
+  int a_ = 0;
+};
+
+template <typename _T>
+struct InnerMessage<_T, true> : public InnerMessage<_T, false> {
+  int b_ = 0;
+};
+
+TEST(sensors, trajectory) {
+
+  InnerMessage<Imu> i1;
+
+  InnerMessage<Gnss> g2;
+
+  EXPECT_EQ(i1.a_, 0);
+  EXPECT_EQ(g2.b_, 0);
+  EXPECT_EQ(g2.a_, 0);
 }

c333dce202b3e94e51b59fec79482c76a7f5aeb6
guanshoukui <guanshoukui@zhito.com>
2024-10-10 07:54:53 +0000

    高频轨迹绘制完成，视口太小时需要优化
    Change-Id: I8d846678c2d6a5faf1a0fedfaec66016bb5959fd

--- src/view/viewer.cpp
+++ src/view/viewer.cpp
@@ -180,41 +180,24 @@ void MyViewer::DownSampleTrajectory(MessageBuffer const& single_buffer, std::vec
   int const rate = 200;
   int last_index = 0; // 上一个被采样索引
   pts_downsample.push_back(raw_pts[0]);
-  int i = rate;
-  for (; i < raw_pts.size(); i += rate) {
+  for (int i = 0; i < raw_pts.size(); i += rate) {
     auto& pt = raw_pts[i];
+    int actual_size = (i + rate < raw_pts.size() ? rate : (raw_pts.size() - i));
+    auto& last_pt = raw_pts[i + actual_size];
 
     // 如果未发生覆盖,从 [last_index,i)
-    double const dx = std::abs(pt.x - pts_downsample.back().x);
-    double const dy = std::abs(pt.y - pts_downsample.back().y);
-    int pixel_n = std::min(rate, (int)(std::hypotf(dx, dy) * scale_ppm)); // 需要这么些个点
+    double const dx = std::abs(pt.x - last_pt.x);
+    double const dy = std::abs(pt.y - last_pt.y);
+    int pixel_n = std::min(actual_size, (int)(std::hypotf(dx, dy) * scale_ppm)); // 需要这么些个点
 
-    // 视口外，忽略
     // 判断last和当前是否在视口内
-    auto& last_pt = raw_pts[i - rate];
     Segmnet_t segment{ Point_t{ last_pt.x, last_pt.y }, Point_t{ pt.x, pt.y } };
 
-    if (!bg::intersects(segment, box)) {
-      // PointLttb::Downsample(&raw_pts[last_index], rate, std::back_inserter(pts_downsample), pixel_n);
-      continue;
-    }
+    if (!bg::intersects(segment, box)) { continue; }
 
     // 在范围且与上一个点的pixel差异在1以上
-    if (pixel_n >= 1) {
-      PointLttb::Downsample(&raw_pts[i - rate], rate, std::back_inserter(pts_downsample), pixel_n);
-      last_index = i;
-    }
+    if (pixel_n >= 1) { PointLttb::Downsample(&raw_pts[i], actual_size, std::back_inserter(pts_downsample), pixel_n); }
   }
-  //! 暂不处理 最后一段尾巴特殊处理，如果前一个点在范围内或者最后一个点在范围
-  // auto& pt = raw_pts.back();
-  // int size = (int)raw_pts.size() - last_index;
-  // auto& mid_pt = raw_pts[i + size / 2];
-  // if (last_index == i || range.Contains(pt) || range.Contains(mid_pt)) {
-  //   double const dx = std::abs(pt.x - pts_downsample.back().x);
-  //   double const dy = std::abs(pt.y - pts_downsample.back().y);
-  //   int pixel_n = std::min(rate, (int)(std::hypotf(dx, dy) * scale_ppm)); // 需要这么些个点
-  //   PointLttb::Downsample(&raw_pts[i], size, std::back_inserter(pts_downsample), pixel_n);
-  // }
 
   ELOGD << "raw raw_pts = " << single_buffer.size() << " downsample pts = " << pts_downsample.size();
 }

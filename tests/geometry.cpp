8bc5cb74847b2a58906f79d758265b2f510b6f3a
guanshoukui <guanshoukui@zhito.com>
2024-10-12 09:02:33 +0000

    添加geometry, eigen测试
    Change-Id: I37774b25c738cfd18edccec6b613cc2b552b33e6

--- tests/geometry.cpp
+++ tests/geometry.cpp
@@ -265,3 +265,107 @@ TEST(geometry, Distance4) {
   auto dis2 = Distance4(p1, line);
   EXPECT_EQ(dis2, 0);
 }
+
+// 使用eigen类型作为自定义类型的点
+template <typename _Scalar, int _dim>
+using EigenPoint = Eigen::Vector<_Scalar, _dim>;
+
+template <typename _Scalar, int _dim>
+class Myline : public Eigen::ParametrizedLine<_Scalar, _dim> {
+  using Eigen::ParametrizedLine<_Scalar, _dim>::ParametrizedLine;
+};
+
+namespace traits {
+
+template <typename _Scalar, int _dim>
+struct access<EigenPoint<_Scalar, _dim>, 0> {
+  _Scalar static get(EigenPoint<_Scalar, _dim> const& pt) { return pt.x(); };
+};
+
+template <typename _Scalar, int _dim>
+struct access<EigenPoint<_Scalar, _dim>, 1> {
+  _Scalar static get(EigenPoint<_Scalar, _dim> const& pt) { return pt.y(); };
+};
+
+template <typename _Scalar, int _dim>
+struct access<EigenPoint<_Scalar, _dim>, 2> {
+  _Scalar static get(EigenPoint<_Scalar, _dim> const& pt) { return pt.z(); };
+};
+
+template <typename _Scalar, int _dim>
+struct tag<EigenPoint<_Scalar, _dim>> {
+  using TagType = PointTag;
+  using coordinate_type = _Scalar;
+  static constexpr int dim = _dim;
+};
+
+template <typename _Scalar, int _dim>
+struct tag<Myline<_Scalar, _dim>> {
+  using TagType = LineTag;
+  using coordinate_type = _Scalar;
+  static constexpr int dim = _dim;
+};
+
+}
+
+// template <typename _T, int _D>
+// double get(_T const& pt) {
+//   return traits::access<_T, _D>::get(pt);
+// }
+
+template <typename _T>
+struct MyTag : public traits::tag<_T> {};
+
+template <typename _T1, typename _T2, int _d>
+struct MyPythagoras {
+
+  using _CommonType = std::common_type_t<typename MyTag<_T1>::coordinate_type, typename MyTag<_T2>::coordinate_type>;
+
+  _CommonType static Apply(_T1 const& p1, _T2 const& p2) {
+    _CommonType d = get<_T1, _d - 1>(p1) - get<_T2, _d - 1>(p2);
+    return d * d + MyPythagoras<_T1, _T2, _d - 1>::Apply(p1, p2);
+  }
+};
+
+template <typename _T1, typename _T2>
+struct MyPythagoras<_T1, _T2, 0> {
+  int static Apply(_T1 const& p1, _T2 const& p2) { return 0; }
+};
+
+namespace dispatcher {
+
+template <typename _Tag1, typename _Tag2, typename _G1, typename _G2>
+struct Distance;
+
+template <typename _G1, typename _G2>
+struct Distance<PointTag, PointTag, _G1, _G2> {
+  static auto Apply(_G1 const& g1, _G2 const& g2) {
+    static_assert(MyTag<_G1>::dim == MyTag<_G2>::dim);
+    return MyPythagoras<_G1, _G2, MyTag<_G1>::dim>::Apply(g1, g2);
+  }
+};
+
+template <typename _G1, typename _G2>
+struct Distance<PointTag, LineTag, _G1, _G2> {
+  static auto Apply(_G1 const& g1, _G2 const& g2) { return g2.distance({ get<_G1, 0>(g1), get<_G1, 1>(g1) }); }
+};
+
+}
+
+template <typename _G1, typename _G2>
+auto Distance5(_G1 const& g1, _G2 const& g2) {
+  return dispatcher::Distance<typename MyTag<_G1>::TagType, typename MyTag<_G2>::TagType, _G1, _G2>::Apply(g1, g2);
+}
+
+TEST(geometry, eigen) {
+  EigenPoint<double, 2> p1{ 0, 0 };
+  EigenPoint<float, 2> p2{ 1, 1 };
+
+  auto dis1 = Distance5(p1, p2);
+
+  EXPECT_EQ(dis1, 2);
+
+  Myline<double, 2> line{ Eigen::Vector2d::Zero(), Eigen::Vector2d::UnitY() };
+  auto dis2 = Distance5(p1, line);
+  EXPECT_EQ(dis2, 0);
+}

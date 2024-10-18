79edbfdfb67b3392f5659594cd21dce3621c7c78
guanshoukui <guanshoukui@zhito.com>
2024-10-18 07:45:05 +0000

    测试0维和1维的几何模型
    Change-Id: I69057961b25b3cee8f3a864250501caf28ff86c7

--- tests/geometry_wrapper.cpp
+++ tests/geometry_wrapper.cpp
@@ -2,6 +2,8 @@
 #include <eigen3/Eigen/Dense>
 #include <gtest/gtest.h>
 
+namespace bg = boost::geometry;
+
 // adapter
 namespace boost {
 namespace geometry {
@@ -48,6 +50,10 @@ struct access<Eigen::Vector<_Scalar, _dim>, 2> {
 }
 }
 } // namespace boost::geometry::traits
+
+template <typename _Scalar, int _dim>
+using MyMultiPoint = boost::geometry::model::multi_point<Eigen::Vector<_Scalar, _dim>>;
+
 TEST(geometry_eigen, adapter_point) {
 
   Eigen::Vector3d p1{ 1, 2, 3 };
@@ -62,4 +68,67 @@ TEST(geometry_eigen, adapter_point) {
 
   auto dis2 = boost::geometry::distance(p1, p2);
   EXPECT_EQ(dis2, 0);
+
+  // multi_point
+  MyMultiPoint<double, 2> mpt;
+  bg::append(mpt, Eigen::Vector2d{ 1, 1 });
+  bg::append(mpt, Eigen::Vector2d{ 2, 2 });
+  EXPECT_EQ(bg::num_points(mpt), 2);
+}
+
+template <typename _Scalar, int _dim>
+using MyLineString = boost::geometry::model::linestring<Eigen::Vector<_Scalar, _dim>>;
+
+TEST(geometry_eigen, MyLineString) {
+
+  MyLineString<double, 2> ls1;
+  ls1.push_back({ 1, 1 });
+  ls1.push_back({ 1, 2 });
+  ls1.push_back({ 2, 2 });
+
+  auto length1 = bg::length(ls1);
+
+  EXPECT_EQ(length1, 2);
+}
+
+template <typename _Scalar, int _dim>
+using MyMultiLineString = boost::geometry::model::multi_linestring<MyLineString<_Scalar, _dim>>;
+
+TEST(geometry_eigen, MyMultiLineString) {
+
+  MyMultiLineString<double, 2> mls;
+  mls.resize(2);
+
+  auto& ls1 = mls[0];
+  ls1.push_back({ 1, 1 });
+  ls1.push_back({ 1, 2 });
+  ls1.push_back({ 2, 2 });
+
+  auto& ls2 = mls[0];
+  ls2.push_back({ 2, 2 });
+  ls2.push_back({ 2, 3 });
+  ls2.push_back({ 3, 3 });
+
+  auto length1 = bg::length(mls);
+
+  EXPECT_EQ(length1, 4);
+}
+
+template <typename _Scalar, int _dim>
+using MySegment = boost::geometry::model::segment<Eigen::Vector<_Scalar, _dim>>;
+
+TEST(geometry_eigen, MySegment) {
+
+  MySegment<double, 3> seg;
+  seg.first = { 1, 1, 1 };
+  seg.second = { 2, 2, 2 };
+
+  auto length = bg::length(seg);
+
+  EXPECT_DOUBLE_EQ(length, sqrt(3));
+
+  // referring_segment
+  bg::model::referring_segment<Eigen::Vector3d> ref_seg(seg.first, seg.second);
+  ref_seg.first = { 3, 3, 3 };
+  EXPECT_TRUE(seg.first.isApprox(ref_seg.first));
 }

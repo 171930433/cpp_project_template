430eb813cc36b1c6085773da81695bd18ec92f10
gsk <171930433@qq.com>
2024-10-28 21:06:24 +0800

    tuple的构造测试完成

--- tests/tuple.cpp
+++ tests/tuple.cpp
@@ -15,6 +15,14 @@ public:
     : head_(head)
     , tail_(tail) {}
 
+  Tuple(_Head const& head, _Tail const&... tail);
+
+  template <typename _VHead, typename... _VTail, typename = std::enable_if_t<(sizeof...(_Tail) == sizeof...(_VTail))>>
+  Tuple(_VHead&& head, _VTail&&... tail);
+
+  template <typename _VHead, typename... _VTail, typename = std::enable_if_t<(sizeof...(_Tail) == sizeof...(_VTail))>>
+  Tuple(Tuple<_VHead, _VTail...> const& other);
+
   _Head const& head() const { return head_; }
   _Head& head() { return head_; }
   Tuple<_Tail...> const& tail() const { return tail_; }
@@ -45,19 +53,58 @@ auto get(Tuple<_Types...> const& t) {
   return TupleGet<_N>::apply(t);
 }
 
-// storage
+// 1. 存储
 TEST(tuple, 25_1_1) {
   Tuple<int, double, std::string> t0{ 1,
     Tuple<double, std::string>{ 1.0, Tuple<std::string>{ "xiaoming", Tuple<>{} } } };
 
-  Tuple<int, double, std::string> t1{ 1, { 1.0, { "xiaoming", {} } } };
-
   EXPECT_EQ(1, get<0>(t0));
   EXPECT_EQ(1.0, get<1>(t0));
   EXPECT_EQ("xiaoming", get<2>(t0));
-  EXPECT_EQ(get<0>(t0), get<0>(t1));
-  EXPECT_EQ(get<1>(t0), get<1>(t1));
-  EXPECT_EQ(get<2>(t0), get<2>(t1));
 }
 
+template <typename _Head, typename... _Tail>
+Tuple<_Head, _Tail...>::Tuple(_Head const& head, _Tail const&... tail)
+  : head_(head)
+  , tail_(tail...) {}
 
+template <typename _Head, typename... _Tail>
+template <typename _VHead, typename... _VTail, typename>
+Tuple<_Head, _Tail...>::Tuple(_VHead&& head, _VTail&&... tail)
+  : head_(std::forward<_VHead>(head))
+  , tail_(std::forward<_VTail>(tail)...) {}
+
+template <typename _Head, typename... _Tail>
+template <typename _VHead, typename... _VTail, typename>
+Tuple<_Head, _Tail...>::Tuple(Tuple<_VHead, _VTail...> const& other)
+  : head_(other.head())
+  , tail_(other.tail()) {}
+
+template <typename... Types>
+auto MakeTuple(Types&&... elems) {
+  return Tuple<std::decay_t<Types>...>(std::forward<Types>(elems)...);
+}
+
+// 2. 构造
+TEST(tuple, 25_1_2) {
+  Tuple<int, double, std::string> t0{ 1, 1.0, "xiaoming" };
+  EXPECT_EQ(1, get<0>(t0));
+  EXPECT_EQ(1.0, get<1>(t0));
+  EXPECT_EQ("xiaoming", get<2>(t0));
+
+  Tuple<long int, long double, std::string> t3(1, 1.0, "xiaoming");
+
+  EXPECT_EQ(1, get<0>(t3));
+  EXPECT_EQ(1.0, get<1>(t3));
+  EXPECT_EQ("xiaoming", get<2>(t3));
+
+  Tuple<long int, long double, std::string> t4(t0);
+  EXPECT_EQ(1, get<0>(t4));
+  EXPECT_EQ(1.0, get<1>(t4));
+  EXPECT_EQ("xiaoming", get<2>(t4));
+
+  auto t5 = MakeTuple(1, 1.0, "xiaoming");
+  EXPECT_EQ(1, get<0>(t5));
+  EXPECT_EQ(1.0, get<1>(t5));
+  EXPECT_EQ("xiaoming", get<2>(t5));
+}

00ea3d5368b7468cddce22182945f3b34dff9317
guanshoukui <guanshoukui@zhito.com>
2024-11-13 07:59:50 +0000

    worked_examples_A 完成
    Change-Id: Ib8ff3327749e558c5772eae97a2d0118841da1f5

--- tests/linear_algbra/chapter_4_1_2.cpp
+++ tests/linear_algbra/chapter_4_1_2.cpp
@@ -26,10 +26,13 @@ TEST_F(Chapter4_1_2, worked_examples_A) {
   Vector3d a1{ 1, 0, 0 };
 
   // 1. 将b投影到a0上
-  Matrix3d P0 = a0 * (a0.transpose() * a0).inverse() * a0.transpose();
-  Vector3d p0 = P0 * b;
+  Matrix3d P_line = a0 * (a0.transpose() * a0).inverse() * a0.transpose();
+  Vector3d p0 = P_line * b;
+  Vector3d e0 = b - p0;
 
-  EXPECT_TRUE((a0.transpose() * (b - p0)).isZero());
+  EXPECT_TRUE((a0.transpose() * (e0)).isZero());
+  EXPECT_TRUE(P_line.isApprox(P_line * P_line));
+  EXPECT_TRUE((P_line * e0).isZero());  // e0在P_line的零空间中
 
   // 2. 将b投影到a0,a1所在的平面
   Matrix<double, 3, 2> A = (Matrix<double, 3, 2>() << a0, a1).finished();
@@ -38,8 +41,11 @@ TEST_F(Chapter4_1_2, worked_examples_A) {
 
   Matrix3d P_plane = A * (A.transpose() * A).inverse() * A.transpose();
   Vector3d p1 = P_plane * b;
+  Vector3d e1 = b - p1;
 
-  EXPECT_TRUE((a0.transpose() * (b - p1)).isZero());
-  EXPECT_TRUE((a1.transpose() * (b - p1)).isZero());
-  EXPECT_TRUE((A.transpose() * (b - p1)).isZero());
+  EXPECT_TRUE((a0.transpose() * (e1)).isZero());
+  EXPECT_TRUE((a1.transpose() * (e1)).isZero());
+  EXPECT_TRUE((A.transpose() * (e1)).isZero());
+  EXPECT_TRUE(P_plane.isApprox(P_plane * P_plane));
+  EXPECT_TRUE((P_plane * e1).isZero()); // e1在p_plane的零空间中
 }

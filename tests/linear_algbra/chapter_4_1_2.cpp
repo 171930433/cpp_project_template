80a1455c40d8560a9f6cdd17f8b05b84b4360f9b
guanshoukui <guanshoukui@zhito.com>
2024-11-14 03:10:11 +0000

    problem_set_4_2_30 添加
    Change-Id: I84e212beb588f8f429050c8d7f402ade30fa0d8c

--- tests/linear_algbra/chapter_4_1_2.cpp
+++ tests/linear_algbra/chapter_4_1_2.cpp
@@ -32,7 +32,7 @@ TEST_F(Chapter4_1_2, worked_examples_A) {
 
   EXPECT_TRUE((a0.transpose() * (e0)).isZero());
   EXPECT_TRUE(P_line.isApprox(P_line * P_line));
-  EXPECT_TRUE((P_line * e0).isZero());  // e0在P_line的零空间中
+  EXPECT_TRUE((P_line * e0).isZero()); // e0在P_line的零空间中
 
   // 2. 将b投影到a0,a1所在的平面
   Matrix<double, 3, 2> A = (Matrix<double, 3, 2>() << a0, a1).finished();
@@ -48,4 +48,68 @@ TEST_F(Chapter4_1_2, worked_examples_A) {
   EXPECT_TRUE((A.transpose() * (e1)).isZero());
   EXPECT_TRUE(P_plane.isApprox(P_plane * P_plane));
   EXPECT_TRUE((P_plane * e1).isZero()); // e1在p_plane的零空间中
+}
+
+TEST_F(Chapter4_1_2, worked_examples_B) {
+  Vector3d A{ 1, 1, 1 };
+  Vector3d b{ 70, 80, 120 };
+
+  Vector<double, 1> x = (A.transpose() * A).inverse() * A.transpose() * b;
+  Vector3d p = A * x;
+  Matrix3d P = A * (A.transpose() * A).inverse() * A.transpose();
+
+  EXPECT_DOUBLE_EQ(x(0), b.mean());
+}
+
+TEST_F(Chapter4_1_2, problem_set_4_2_1) {
+  Vector3d b1{ 1, 2, 2 };
+  Vector3d a1{ 1, 1, 1 };
+
+  double x = (a1.transpose() * a1).inverse() * a1.transpose() * b1;
+  Vector3d p1 = a1 * x;
+  Vector3d e1 = b1 - p1;
+  Matrix3d P1 = a1 * (a1.transpose() * a1).inverse() * a1.transpose();
+
+  EXPECT_TRUE((P1 * b1).isApprox(p1));
+  EXPECT_TRUE((a1.transpose() * e1).isZero());
+
+  //
+  Vector3d b2{ 1, 2, 2 };
+  Vector3d a2{ 1, 1, 1 };
+  double x2 = (a2.transpose() * a2).inverse() * a2.transpose() * b2;
+  Vector3d p2 = a2 * x2;
+  Vector3d e2 = b2 - p2;
+  Matrix3d P2 = a2 * (a2.transpose() * a2).inverse() * a2.transpose();
+
+  EXPECT_TRUE((P2 * b2).isApprox(p2));
+  EXPECT_TRUE((a2.transpose() * e2).isZero());
+}
+
+TEST_F(Chapter4_1_2, problem_set_4_2_30) {
+  Matrix<double, 2, 3> A = (Matrix<double, 2, 3>() << 3, 6, 6, 4, 8, 8).finished();
+
+  EXPECT_EQ(A.fullPivLu().rank(), 1);
+  EXPECT_EQ(A.fullPivLu().dimensionOfKernel(), A.cols() - 1);
+
+  Matrix<double, 2, 1> A1 = A.col(0);
+  Matrix2d Pc = A1 * (A1.transpose() * A1).inverse() * A1.transpose();
+
+  Matrix<double, 3, 1> A2 = A.transpose().col(0);
+
+  Matrix3d Pr = A2 * (A2.transpose() * A2).inverse() * A2.transpose();
+
+  ELOGD << "A is \n" << A;
+  ELOGD << "Pc is \n" << Pc;
+  ELOGD << "Pr is \n" << Pr;
+
+  Matrix<double, 2, 3> B = Pc * A * Pr;
+  ELOGD << "B is \n" << B;
+
+  // Pc * A 是将A的列向量往A的主列投影，而A的秩为1，所以所有列都与主列线性相关，所以
+  // Pc * A = A
+  EXPECT_TRUE((Pc * A).isApprox(A));
+
+  // 同理
+  EXPECT_TRUE((A * Pr).isApprox(A));
+  EXPECT_TRUE((Pc * A * Pr).isApprox(A));
 }

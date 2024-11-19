d642b00faf097a0c5476fce502a7e466f73f7150
gsk <171930433@qq.com>
2024-11-19 14:00:15 +0800

    Chapter4_4_ProbelemSet_30_34 测试完成

--- tests/linear_algbra/chapter_4_4.cpp
+++ tests/linear_algbra/chapter_4_4.cpp
@@ -3,7 +3,8 @@
 #include <gtest/gtest.h>
 
 // 4.4 gram-schmidt 正交化
-//
+// 正交向量 q.transpose() * p = 0
+// 正交矩阵 Q是方阵， Q.transpose() * Q = I, 基Qinv=Q.transpose()
 
 using namespace Eigen;
 
@@ -69,3 +70,53 @@ TEST_F(Chapter4_4, test_QR2) {
 
   ELOGD << " qr.householderQ = \n" << qr.householderQ() * Matrix3d::Identity();
 }
+
+TEST_F(Chapter4_4, problem_set_11) {
+  Vector<double, 5> a = (Vector<double, 5>() << 1, 3, 4, 5, 7).finished();
+  Vector<double, 5> b = (Vector<double, 5>() << -6, 6, 8, 0, 8).finished();
+  Vector<double, 5> c = (Vector<double, 5>() << 1, 0, 0, 0, 0).finished();
+  Matrix<double, 5, 2> A = (Matrix<double, 5, 2>() << a, b).finished();
+
+  Vector<double, 5> q0 = a.normalized();
+  Vector<double, 5> q1 = (b - q0.transpose() * b * q0).normalized();
+
+  auto const& [Q, R] = QRDecomposition(A);
+
+  EXPECT_TRUE(Q.col(0).isApprox(q0));
+  EXPECT_TRUE(Q.col(1).isApprox(q1));
+
+  // 求解c在ab的列空间投影点
+  Vector<double, 5> p = Q * Q.transpose() * c;
+  Vector<double, 5> p0 = q0.transpose() * c * q0;
+  Vector<double, 5> p1 = q1.transpose() * c * q1;
+
+  EXPECT_TRUE(p.isApprox(p0 + p1));
+
+  EXPECT_TRUE(p.isApprox((Vector<double, 5>() << 0.5, -0.18, -0.24, 0.4, 0).finished()));
+}
+
+class Chapter4_4_ProbelemSet_30_34 : public testing::Test {
+
+public:
+  Chapter4_4_ProbelemSet_30_34() {
+    double s2 = sqrt(2);
+    W << 1, 1, s2, 0, 1, 1, -s2, 0, 1, -1, 0, s2, 1, -1, 0, -s2;
+    W *= 0.5;
+    Q << 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, -1, -1, 1;
+  }
+  void SetUp() override {}
+
+  void TearDown() override {}
+
+protected:
+  Matrix4d W;
+  Matrix4d Q;
+};
+
+TEST_F(Chapter4_4_ProbelemSet_30_34, 30) {
+  // W的列正交，行也正交，行列向量的模值都是1
+  Matrix4d Winv = W.inverse();
+  EXPECT_TRUE(W.transpose().isApprox(Winv));
+}
+
+TEST_F(Chapter4_4_ProbelemSet_30_34, 31) { Vector4d b = { 1, 1, 1, 1 }; }

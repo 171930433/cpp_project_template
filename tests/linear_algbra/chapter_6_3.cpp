5aa40debf28d33876c34d4219d728258ff787b74
gsk <171930433@qq.com>
2024-11-29 18:03:11 +0800

    添加6.3problem_set_1测试用例

--- tests/linear_algbra/chapter_6_3.cpp
+++ tests/linear_algbra/chapter_6_3.cpp
@@ -1,4 +1,5 @@
 #include <eigen3/Eigen/Dense>
+#include <eigen3/unsupported/Eigen/MatrixFunctions>
 #include <gtest/gtest.h>
 #include <ylt/easylog.hpp>
 
@@ -55,4 +56,39 @@ TEST_F(Chapter6_3, eigenvalues_eigenvectors2) {
   Matrix2d S = solver.eigenvectors();
 
   ELOGD << " S = \n" << S;
+}
+
+TEST_F(Chapter6_3, problem_set_1) {
+
+  Matrix2d A = (Matrix2d() << 4, 3, 0, 1).finished();
+
+  Eigen::EigenSolver<Matrix2d> solver(A);
+
+  Vector2d u0{ 5, -2 };
+
+  Matrix2d S = solver.eigenvectors().real();
+  Vector2d lambdas = solver.eigenvalues().real();
+
+  Vector2d c = S.colPivHouseholderQr().solve(u0);
+
+  EXPECT_TRUE((S * c).isApprox(u0));
+
+  auto ut = [S, lambdas, c](
+              int t) { return c(0) * exp(lambdas(0) * t) * S.col(0) + c(1) * exp(lambdas(1) * t) * S.col(1); };
+
+  EXPECT_TRUE(ut(0).isApprox(u0));
+  ELOGD << " ut(0) = " << ut(0).transpose() << " u0 = " << u0.transpose();
+
+  auto ut2 = [A, u0](int t) -> Vector2d { return (A * t).exp() * u0; };
+  auto ut3 = [S, lambdas, c](int t) -> Vector2d { return S * (lambdas.asDiagonal().toDenseMatrix() * t).exp() * c; };
+
+  ELOGD << " ut(1) = " << ut(1).transpose() << " ut2(1) = " << ut2(1).transpose();
+
+  ELOGD << " ut(2) = " << ut(2).transpose() << " ut2(2) = " << ut2(2).transpose();
+  ELOGD << " ut(3) = " << ut(3).transpose() << " ut2(3) = " << ut2(3).transpose();
+
+  EXPECT_TRUE((ut(1).isApprox(ut2(1))));
+  EXPECT_TRUE((ut(2).isApprox(ut2(2))));
+  EXPECT_TRUE((ut(3).isApprox(ut2(3))));
+  EXPECT_TRUE((ut(3).isApprox(ut3(3))));
 }
